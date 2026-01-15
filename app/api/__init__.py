c764dfdcdf1b50a1c380801a707b01681d00c979
Foreign A Foot Logistics <foreigafootlogistics@gmail.com>
2026-01-15 17:50:17 -0500

    Rename mobile API blueprint to avoid name conflict

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-api_bp = Blueprint("api", __name__, url_prefix="/api")
+api_bp = Blueprint("mobile_api", __name__, url_prefix="/api")
 
 from . import auth, me, utils  # import submodules
 
